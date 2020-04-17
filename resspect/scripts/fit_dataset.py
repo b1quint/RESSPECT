502b61ad6c0086624799f81b181ea593ad17803a
emilleishida <emilleishida@gmail.com>
2020-04-17 10:47:47 +0200

    change names

--- resspect/scripts/fit_dataset.py
+++ resspect/scripts/fit_dataset.py
@@ -1,8 +1,7 @@
-# Copyright 2019 actsnclass software
+# Copyright 2020 resspect software
 # Author: Emille E. O. Ishida
-#         Based on initial prototype developed by the CRP #4 team
 #
-# created on 8 August 2019
+# created on 14 April 2020
 #
 # Licensed GNU General Public License v3.0;
 # you may not use this file except in compliance with the License.
@@ -18,7 +17,9 @@
 
 import argparse
 
-from actsnclass.fit_lightcurves import fit_snpcc_bazin
+from resspect.fit_lightcurves import fit_snpcc_bazin
+from resspect.fit_lightcurves import fit_resspect_bazin
+from resspect.fit_lightcurves import fit_plasticc_bazin
 
 __all__ = ['main']
 
@@ -30,25 +31,52 @@ def main(user_choices):
 
     Parameters
     ----------
-    -dd: str
-        Path to directory containing raw data.
     -o: str
         Path to output feature file.
+    -s: str
+        Simulation name. Options are 'SNPCC', 'RESSPECT' or 'PLAsTiCC'.
+    -dd: str (optional)
+        Path to directory containing raw data.
+        Only used for SNPCC simulations.
+    -hd: str (optional)
+        Path to header file. Only used for RESSPECT simulations.
+    -p: str (optional)
+        Path to photometry file. Only used for RESSPECT simulations.
+    -sp: str or None (optional)
+        Sample to be fitted. Options are 'train', 'test' or None.
+        Default is None.
 
     Examples
     --------
 
-    Run directly from the command line.
+    For SNPCC: 
 
-    >>> fit_dataset.py -dd <path_to_data_dir> -o <output_file>
+    >>> fit_dataset.py -s SNPCC -dd <path_to_data_dir> -o <output_file>
+
+    For RESSPECT or PLAsTiCC:
+
+    >>> fit_dataset.py -s <dataset_name> -p <path_to_photo_file> 
+             -hd <path_to_header_file> -o <output_file> 
     """
 
     # raw data directory
     data_dir = user_choices.input
     features_file = user_choices.output
 
-    # fit the entire sample
-    fit_snpcc_bazin(path_to_data_dir=data_dir, features_file=features_file)
+    if user_choices.sim_name == 'SNPCC':
+        # fit the entire sample
+        fit_snpcc_bazin(path_to_data_dir=data_dir, features_file=features_file)
+    
+    elif user_choices.sim_name == 'RESSPECT':
+        fit_resspect_bazin(path_photo_file=user_choices.photo_file,
+                           path_header_file=user_choices.header_file,
+                           output_file=features_file, sample=user_choices.sample)
+
+    elif user_choices.sim_name == 'PLAsTiCC':
+        fit_plasticc_bazin(path_photo_file=user_choices.photo_file, 
+                           path_header_file=user_choices.header_file,
+                           output_file=features_file,
+                           sample=user_choices.sample)
 
     return None
 
@@ -56,10 +84,27 @@ def main(user_choices):
 if __name__ == '__main__':
 
     # get input directory and output file name from user
-    parser = argparse.ArgumentParser(description='actsnclass - Fit Light curves module')
+    parser = argparse.ArgumentParser(description='resspect - Fit Light curves module')
+   
     parser.add_argument('-dd', '--datadir', dest='input',
-                        help='Path to directory holding raw data.', required=True)
-    parser.add_argument('-o', '--output', dest='output', help='Path to output file.', required=True)
+                        help='Path to directory holding raw data. Only used for SNPCC',
+                        required=False, default=' ')
+    parser.add_argument('-hd', '--header', dest='header_file', 
+                        help='Path to header file. Only used for RESSPECT.',
+                        required=False, default=' ')
+    parser.add_argument('-o', '--output', dest='output', help='Path to output file.', 
+                        required=True)
+    parser.add_argument('-p', '--photo', dest='photo_file',
+                        help='Path to photometry file. Only used for RESSPECT.',
+                        required=False, default=' ')
+    parser.add_argument('-s', '--simulation', dest='sim_name', 
+                        help='Name of simulation (data set). ' + \
+                             'Options are "SNPCC", "RESSPECT" or "PLAsTiCC".',
+                        required=True)
+    parser.add_argument('-sp', '--sample', dest='sample',
+                        help='Sample to be fitted. Options are "train", ' + \
+                             ' "test" or None.',
+                        required=False, default=None)
 
     user_input = parser.parse_args()
 
